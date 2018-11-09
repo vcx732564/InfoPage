fcd67042c65fdbe431bfa4ac5f4501352be42d3f
vcx732564 <vcx732564@hotmail.com>
2018-11-09 18:01:50 +0800

    2018-11-09 Bensonwork Change 她媽的 info 配舉

--- InfoPageCode/Scripts/InfoPage_JS.js
+++ InfoPageCode/Scripts/InfoPage_JS.js
@@ -8,8 +8,8 @@ $("ul.tabs li").click(function () {
     $(".tab_content").hide();
     var activeTab = $(this).attr("rel");
     $("#" + activeTab).fadeIn();
-    //變動高度
-    $(".Info_Page").height(String(500 + parseInt($("#" + activeTab).height()) + 30) + 'px');
+    //變動高度 桌面模式不變動高度
+    //$(".Info_Page").height(String(500 + parseInt($("#" + activeTab).height()) + 30) + 'px');
     //$(".Info_Page").animate({ "height": String(500 + parseInt($("#" + activeTab).height()) + 30) + 'px' });
 
 
@@ -26,13 +26,15 @@ $(".tab_drawer_heading").click(function () {
     //判斷目前文字框是否是開啟狀態
     var now_Text_Area = $(this).attr('rel');
     if ($('#' + now_Text_Area).is(":hidden")) {
-        $(".tab_content").hide();
+        $(".tab_content").fadeOut();
+
         var d_activeTab = $(this).attr("rel");
         $("#" + d_activeTab).fadeIn();
-        //變動高度
-        $(".Info_Page").height(String(500 + parseInt($("#" + d_activeTab).height()) + 30) + 'px');
+        
+        //變動高度 行動模式才要變動高度
+        //$(".Info_Page").height(String(500 + parseInt($("#" + d_activeTab).height()) + 30) + 'px');
+        $(".Info_Page").animate({ height: 500 + parseInt($("#" + d_activeTab).height()) + 30 + 'px' });
         //$(".Info_Page").animate({"height": String(500 + parseInt($("#" + activeTab).height()) + 30) + 'px'});
-
 
         $(".tab_drawer_heading").removeClass("d_active");
         $(this).addClass("d_active");
