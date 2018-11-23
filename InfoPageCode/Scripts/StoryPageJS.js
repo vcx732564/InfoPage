530d0603c489f797bed6e3a4eb1830b6755d6cd3
vcx732564 <vcx732564@hotmail.com>
2018-11-23 15:14:06 +0800

    2018-11-23 Bensonwork 增加個人自傳

---
+++ InfoPageCode/Scripts/StoryPageJS.js
@@ -0,0 +1,66 @@
+﻿// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    $.ajax({
+        //要求資料的網頁
+        url: "./TextFile/Test.txt?a=4",
+        //非同步處理 這要加 很重要
+        async: false,
+        //格式
+        dataType: "text",
+        //成功時做的事情
+        success: function (data) {
+            $("#TestTxt").html(data);
+            $("#testh2").html("第一章");
+        },
+        //失敗時做的事情
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.responseText);
+        }
+    });
+    modal.style.display = "block";
+
+}
+
+$("#myBtn2").click(function () {
+
+    $.ajax({
+        //要求資料的網頁
+        url: "./TextFile/Test2.txt?a=4",
+        //非同步處理 這要加 很重要
+        async: false,
+        //格式
+        dataType: "text",
+        //成功時做的事情
+        success: function (data) {
+            $("#TestTxt").html(data);
+            $("#testh2").html("第二章");
+        },
+        //失敗時做的事情
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.responseText);
+        }
+    });
+    modal.style.display = "block";
+});
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
