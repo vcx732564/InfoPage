36b0adb4dddae2a8f1a555583dc31181a1818a2a
vcx732564 <vcx732564@hotmail.com>
2018-12-21 15:33:10 +0800

    2018-12-21 Bensonwork Story那邊他媽增加一些鬼東西

--- InfoPageCode/Scripts/StoryPageJS.js
+++ InfoPageCode/Scripts/StoryPageJS.js
@@ -1,59 +1,39 @@
 ﻿// Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("StoryChapter1");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-    $.ajax({
-        //要求資料的網頁
-        url: "./TextFile/Test.txt?a=4",
-        //非同步處理 這要加 很重要
-        async: false,
-        //格式
-        dataType: "text",
-        //成功時做的事情
-        success: function (data) {
-            $("#StoryContent").html(data);
-            $("#chapter_title").html("第一章");
-            $("#chapter_content").html("出生至國小");
-        },
-        //失敗時做的事情
-        error: function (xhr, ajaxOptions, thrownError) {
-            alert(xhr.responseText);
-        }
-    });
-    modal.style.display = "block";
-    return true;
-}
+$("div[id^=StoryChapter]").each(function (index) {
+    //點擊事件
+    $(this).click(function (index) {
 
-$("#myBtn2").click(function () {
+        //ID參數
+        var objId = $(this).attr('id');
 
-    $.ajax({
-        //要求資料的網頁
-        url: "./TextFile/Test2.txt?a=4",
-        //非同步處理 這要加 很重要
-        async: false,
-        //格式
-        dataType: "text",
-        //成功時做的事情
-        success: function (data) {
-            $("#StoryContent").html(data);
-            $("#chapter_title").html("第二章");
-            $("#chapter_content").html("國中至高中時期");
-        },
-        //失敗時做的事情
-        error: function (xhr, ajaxOptions, thrownError) {
-            alert(xhr.responseText);
-        }
+        //NowTime 是無意義參數 用來防止瀏覽器吃舊的暫存
+        var NowTime = Date.now();
+
+        $.ajax({
+            //要求資料的路徑
+            url: "./TextFile/" + objId + "_Text.txt?NowTime=" + NowTime,
+            //非同步處理 這要加 很重要
+            async: false,
+            //格式
+            dataType: "text",
+            //成功時做的事情
+            success: function (data) {
+                $("#StoryContent").html(data);
+            },
+            //失敗時做的事情
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.responseText);
+            }
+        });
+        modal.style.display = "block";
     });
-    modal.style.display = "block";
+
 });
-
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
@@ -67,9 +47,6 @@ window.onclick = function (event) {
     }
 }
 
-
-
-
 //網頁滾動時要做的動畫
 $(window).scroll(function () {
     if ($(this).scrollTop() > $("div[class=StoryPage]").offset().top - 150) {
@@ -77,9 +54,9 @@ $(window).scroll(function () {
         $('div[id^=StoryChapter]').each(function () {
 
             var ShowSpeed = parseInt($(this).attr('Showspeed'));
-            
+
             if ($(this).attr('fadeIn') == "N") {
-                
+
                 $(this).animate({
                     opacity: 1,
                     //left: '-=30'
@@ -88,20 +65,17 @@ $(window).scroll(function () {
                     //$(this).animate({ "opacity": "0.8" });
                     //$(this).removeClass('StoryChapterOut');
                     //$(this).addClass('StoryChapterIn');
-                    
                 });
                 $(this).attr('fadeIn', 'Y');
-               
             }
         });
-
     } else {
         $('div[id^=StoryChapter]').each(function () {
 
             var ShowSpeed = parseInt($(this).attr('Showspeed'));
-           
+
             if ($(this).attr('fadeIn') == "Y") {
-                
+
                 $(this).animate({
                     opacity: 0,
                     //left: "+=30"
@@ -110,14 +84,11 @@ $(window).scroll(function () {
                     //$(this).animate({ "opacity": "0" });
                     //$(this).removeClass('StoryChapterIn');
                     //$(this).addClass('StoryChapterOut');
-                    
+
                 });
                 $(this).attr('fadeIn', 'N');
-                
             }
-
         });
-
     }
 });
 
