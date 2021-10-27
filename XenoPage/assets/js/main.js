f01ba99e52b98da6481e0f61e4b488cf1a6d86f1
vcx732564 <vcx732564@hotmail.com>
2021-10-27 23:46:26 +0800

    2021-10-27 final ex

---
+++ XenoPage/assets/js/main.js
@@ -0,0 +1,48 @@
+﻿/* Nav bar js*/
+$('#menubtn').click(function () {
+    $('.leftmenu').toggleClass('active');
+})
+
+$('#menuUl li').click(function () {
+    $(this).parent().find('li').each(function () {
+        if ($(this).hasClass('current-active')) {
+            $(this).toggleClass('current-active');
+        }
+    })
+    $(this).toggleClass('current-active');
+})
+
+$('.closebtn').click(function () {
+    $('.leftmenu').toggleClass('active');
+})
+
+
+/**
+   * Hero type effect
+   */
+    (function () {
+        "use strict";
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+        return [...document.querySelectorAll(el)]
+    } else {
+        return document.querySelector(el)
+    }
+}
+
+
+const typed = select('.typed')
+if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+    });
+        }
+
+    })()
