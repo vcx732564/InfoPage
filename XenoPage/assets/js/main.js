dac96680e664c751f5300a5def43688671d76bbc
vcx732564 <vcx732564@hotmail.com>
2022-03-03 19:34:00 +0800

    2022-03-03

--- XenoPage/assets/js/main.js
+++ XenoPage/assets/js/main.js
@@ -20,8 +20,7 @@ $('.closebtn').click(function () {
 /**
    * Hero type effect
    */
-    (function () {
-        "use strict";
+
 const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -32,7 +31,8 @@ const select = (el, all = false) => {
 }
 
 
-const typed = select('.typed')
+
+const typed = select('.typed');
 if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
     typed_strings = typed_strings.split(',')
@@ -43,6 +43,25 @@ if (typed) {
         backSpeed: 50,
         backDelay: 2000
     });
-        }
+}
 
-    })()
+
+
+/**
+ * Skills animation
+ */
+let skilsContent = select('.skills-content');
+if (skilsContent) {
+    new Waypoint({
+        element: skilsContent,
+        offset: '100%',
+        handler: function (direction) {
+            let progress = select('.progress .progress-bar', true);
+            progress.forEach((el) => {
+                el.style.width = el.getAttribute('aria-valuenow') + '%'
+            });
+        }
+    })
+}
+
+
